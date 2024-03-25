08162947bd05f546364f421acdcdb6d896dba00b
Ananth Narayan <narayanananth26@gmail.com>
2024-03-25 22:17:18 +0530

    feat: add middleware to protect routes

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-import { db } from "./lib/db";
+import authConfig from "@/auth.config";
+import { db } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const {
 	handlers: { GET, POST },
 	auth,
+	signIn,
+	signOut,
 } = NextAuth({
 	adapter: PrismaAdapter(db),
 	session: { strategy: "jwt" },
