11434279636f8b95f3ee5c46d1f9edb510a067eb
Ananth Narayan <narayanananth26@gmail.com>
2024-03-25 22:16:50 +0530

    chore: create settings protected route

--- src/routes.ts
+++ src/routes.ts
@@ -2,22 +2,22 @@
  * Array of public routes.
  * @type {string[]}
  */
-export const publicRoutes = ["/"];
+export const publicRoutes: string[] = ["/"];
 
 /**
  * Array of routes that require authentication.
  * @type {string[]}
  */
-export const authRoutes = ["/auth/login", "/auth/register"];
+export const authRoutes: string[] = ["/auth/login", "/auth/register"];
 
 /**
  * Prefix for API authentication routes.
  * @type {string}
  */
-export const apiAuthPrefix = "/api/auth";
+export const apiAuthPrefix: string = "/api/auth";
 
 /**
  * The default login redirect route.
  * @type {string}
  */
-export const DEFAULT_LOGIN_REDIRECT = "/";
+export const DEFAULT_LOGIN_REDIRECT: string = "/settings";
