c54dfb5f399128a99003a5f4f09c9f8485b3d3db
Ananth Narayan <narayanananth26@gmail.com>
2024-05-01 16:38:24 +0530

    feat: add middleware for route protection

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,23 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/anyone-can-visit-this-route'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
-});
- 
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+	const path = req.nextUrl.pathname;
+	const token = await getToken({
+		req: req,
+		secret: process.env.NEXTAUTH_SECRET,
+	});
+
+	const publicPaths = path === "/login" || path === "/signup";
+
+	if (publicPaths && token) {
+		return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+	}
+	if (!publicPaths && !token) {
+		return NextResponse.redirect(new URL("/", req.nextUrl));
+	}
+}
+
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: ["/", "/login", "/signup", "/dashboard"],
 };
-
-
-
