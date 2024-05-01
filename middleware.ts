5c707b2c455ef89b296c6d202f4b3dd40d90efea
Ananth Narayan <narayanananth26@gmail.com>
2024-05-01 17:02:23 +0530

    chore: update middleware

--- middleware.ts
+++ middleware.ts
@@ -11,11 +11,16 @@ export async function middleware(req: NextRequest) {
 	const publicPaths = path === "/login" || path === "/signup";
 
 	if (publicPaths && token) {
+		// If the user is authenticated and tries to access public paths, redirect them to the dashboard
 		return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
-	}
-	if (!publicPaths && !token) {
+	} else if (!publicPaths && !token) {
+		// If the user is not authenticated and tries to access private paths, redirect them to the homepage
 		return NextResponse.redirect(new URL("/", req.nextUrl));
 	}
+
+	// If the user is authenticated and tries to access private paths or if the user is not authenticated and tries to access public paths,
+	// let the request pass through
+	return null;
 }
 
 export const config = {
